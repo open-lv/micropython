d2a00498d498f8245fb59e802b62493c97bca14e
Reinis Veips <reinis.veips@wot.lv>
2022-02-12 15:54:08 +0200

    Decrease mPy heap size by 36KiB, so that enough memory remains for lib_deflate_reader

--- ports/esp32/main.c
+++ ports/esp32/main.c
@@ -142,7 +142,7 @@ void mp_task(void *pvParameter) {
     }
     #else
     // Allocate the uPy heap using malloc and get the largest available region
-    size_t mp_task_heap_size = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT);
+    size_t mp_task_heap_size = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT) - 36*1024;
     void *mp_task_heap = malloc(mp_task_heap_size);
     #endif
 
